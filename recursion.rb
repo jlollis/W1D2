870121cc6f3a07c99dd711e3608ca5a355996aa1
Julie Lollis <julielollis@gmail.com>
2019-02-19 13:17:37 -0500

    add html containers and first recursion exercise

---
+++ recursion.rb
@@ -0,0 +1,82 @@
+# Exercise 1
+# Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
+
+def sum_to(min=1, n)
+  if n > 0
+    return min if min == n 
+    min + sum_to(min + 1, n)
+  end
+end
+
+p sum_to(5)  # => returns 15
+p sum_to(1)  # => returns 1
+p sum_to(9)  # => returns 45
+p sum_to(-8)  # => returns nil --> stack level too deep error at first, added conditional to filter out negative values and now as expected.
+
+# Exercise 2
+# Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers. Write this method recursively.
+
+def add_numbers(nums_array)
+  if nums_array == []
+    return 0
+  else 
+    n = nums_array.pop
+    return n + add_numbers(nums_array)
+  end 
+end
+
+# Test Cases
+p add_numbers([1,2,3,4]) # => returns 10
+p add_numbers([3]) # => returns 3
+p add_numbers([-80,34,7]) # => returns -39
+# puts add_numbers([]) # => returns nil --> Dang, can't get error handling to work, can't break or return nil...moving on...
+
+# Exercise 3
+# Let's write a method that will solve Gamma Function recursively. The Gamma Function is defined Γ(n) = (n-1)!.
+
+class Integer
+  def fact
+    (1..self).inject(:*) || 1
+  end
+end
+
+def gamma_fnc(n)
+  if n != 0 
+    (n-1).fact
+  end
+end
+
+# Test Cases
+p gamma_fnc(0)  # => returns nil
+p gamma_fnc(1)  # => returns 1
+p gamma_fnc(4)  # => returns 6
+p gamma_fnc(8)  # => returns 5040
+
+# Exercise 4
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not the shop offers their favorite flavor.
+
+def ice_cream_shop(flavors, favorite)
+  flavors.select{|word| word == favorite }.empty? == false 
+end
+
+# Test Cases
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
+
+# Exercise 5
+# Write a function reverse(string) that takes in a string and returns it reversed.
+
+def reverse(string)
+  string.length < 2 ? string : string[-1] + reverse(string[1..-2]) + string[0]
+end
+
+# Test Cases
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
